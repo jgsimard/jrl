ffd1bf7bd99e38fa294d2769ea75fe4bb3685e19
jgsimard <jeangab23@gmail.com>
2022-09-08 22:04:44 -0400

    reformat make_agent

--- train.py
+++ train.py
@@ -11,13 +11,11 @@ from omegaconf import DictConfig, OmegaConf
 from tensorboardX import SummaryWriter
 # from dm_env import specs
 
-from agents.td3 import TD3
-from agents.sac import SAC
-from agents.drq import DrQ
 from data.replay_buffer import ReplayBuffer
 # from data.replay_buffer_compressed import ReplayBufferStorage, make_replay_loader
 from common.env.utils import make_env
 from common.evaluation import evaluate
+from utils import make_agent
 
 warnings.filterwarnings("ignore")  # TODO remove this
 
@@ -30,59 +28,6 @@ PLANET_ACTION_REPEAT = {
     'ball_in_cup-catch': 4,
     'walker-walk': 2
 }
-
-
-def make_agent(params, env):
-    agent_params = params['agent']
-    agent_name = agent_params['name']
-    if agent_name == 'TD3':
-        agent = TD3(
-            params['seed'],
-            env.observation_space.sample()[np.newaxis],
-            env.action_space.sample()[np.newaxis],
-            actor_lr=agent_params['actor_lr'],
-            critic_lr=agent_params['critic_lr'],
-            exploration_noise=agent_params['exploration_noise'],
-            policy_noise=agent_params['exploration_noise'],
-            noise_clip=agent_params['noise_clip']
-        )
-    elif agent_name == 'SAC':
-        agent = SAC(
-            params['seed'],
-            env.observation_space.sample()[np.newaxis],
-            env.action_space.sample()[np.newaxis],
-            actor_lr=agent_params['actor_lr'],
-            critic_lr=agent_params['critic_lr'],
-            temperature_lr=agent_params['temperature_lr']
-        )
-    elif agent_name == 'DroQ':
-        agent = SAC(
-            params['seed'],
-            env.observation_space.sample()[np.newaxis],
-            env.action_space.sample()[np.newaxis],
-            actor_lr=params['DroQ']['actor_lr'],
-            critic_lr=params['DroQ']['critic_lr'],
-            temperature_lr=params['DroQ']['temperature_lr'],
-            layer_norm=params['DroQ']['layer_norm'],
-            dropout_rate=params['DroQ']['dropout_rate'],
-        )
-    elif agent_name == 'DrQ':
-        agent = DrQ(
-            params['seed'],
-            env.observation_space.sample()[np.newaxis],
-            env.action_space.sample()[np.newaxis],
-            hidden_dims=agent_params['hidden_dims'],
-            cnn_features=agent_params['cnn_features'],
-            cnn_strides=agent_params['cnn_strides'],
-            cnn_padding=agent_params['cnn_padding'],
-            latent_dim=agent_params['latent_dim'],
-            actor_lr=agent_params['actor_lr'],
-            critic_lr=agent_params['critic_lr'],
-            temperature_lr=agent_params['temperature_lr']
-        )
-    else:
-        raise NotImplementedError(f"Agent {agent_name} not implemented yet")
-    return agent
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
---
+++ utils.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from agents.drq import DrQ
+from agents.sac import SAC
+from agents.td3 import TD3
+
+
+def make_agent(params, env):
+    agent_params = params['agent']
+    agent_name = agent_params['name']
+    if agent_name == 'TD3':
+        agent = TD3(
+            params['seed'],
+            env.observation_space.sample()[np.newaxis],
+            env.action_space.sample()[np.newaxis],
+            actor_lr=agent_params['actor_lr'],
+            critic_lr=agent_params['critic_lr'],
+            exploration_noise=agent_params['exploration_noise'],
+            policy_noise=agent_params['exploration_noise'],
+            noise_clip=agent_params['noise_clip']
+        )
+    elif agent_name == 'SAC':
+        agent = SAC(
+            params['seed'],
+            env.observation_space.sample()[np.newaxis],
+            env.action_space.sample()[np.newaxis],
+            actor_lr=agent_params['actor_lr'],
+            critic_lr=agent_params['critic_lr'],
+            temperature_lr=agent_params['temperature_lr']
+        )
+    elif agent_name == 'DroQ':
+        agent = SAC(
+            params['seed'],
+            env.observation_space.sample()[np.newaxis],
+            env.action_space.sample()[np.newaxis],
+            actor_lr=params['DroQ']['actor_lr'],
+            critic_lr=params['DroQ']['critic_lr'],
+            temperature_lr=params['DroQ']['temperature_lr'],
+            layer_norm=params['DroQ']['layer_norm'],
+            dropout_rate=params['DroQ']['dropout_rate'],
+        )
+    elif agent_name == 'DrQ':
+        agent = DrQ(
+            params['seed'],
+            env.observation_space.sample()[np.newaxis],
+            env.action_space.sample()[np.newaxis],
+            hidden_dims=agent_params['hidden_dims'],
+            cnn_features=agent_params['cnn_features'],
+            cnn_strides=agent_params['cnn_strides'],
+            cnn_padding=agent_params['cnn_padding'],
+            latent_dim=agent_params['latent_dim'],
+            actor_lr=agent_params['actor_lr'],
+            critic_lr=agent_params['critic_lr'],
+            temperature_lr=agent_params['temperature_lr']
+        )
+    else:
+        raise NotImplementedError(f"Agent {agent_name} not implemented yet")
+    return agent
